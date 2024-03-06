916d90ef2755ab17664e74d46d7689a17b61b0c1
Carolaine Machado <carolainemachado12345@gmail.com>
2024-03-06 13:05:13 -0300

    Criando operacao de instanciacao

--- src/main/java/com/carolaine/cursomod_conceitual/CursomodConceitualApplication.java
+++ src/main/java/com/carolaine/cursomod_conceitual/CursomodConceitualApplication.java
@@ -1,13 +1,32 @@
 package com.carolaine.cursomod_conceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomodConceitualApplication {
+import com.carolaine.cursomod_conceitual.domain.Categoria;
+import com.carolaine.cursomod_conceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomodConceitualApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomodConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria (null, "Informática");
+		Categoria cat2 = new Categoria (null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
