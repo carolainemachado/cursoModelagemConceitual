35403677e02b78da9681a3329376ec4df6f3fee5
Carolaine Machado <carolainemachado12345@gmail.com>
2024-03-06 10:49:08 -0300

    Projeto criado

---
+++ src/main/java/com/carolaine/cursomod_conceitual/CursomodConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.carolaine.cursomod_conceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomodConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomodConceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carolaine/cursomod_conceitual/CursomodConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolaine.cursomod_conceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomodConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
