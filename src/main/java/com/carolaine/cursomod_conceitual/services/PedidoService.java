c7dba4d104f683a0b207ef499c2644140c98e1fa
Carolaine Machado <carolainemachado12345@gmail.com>
2024-03-07 17:22:55 -0300

    Endpoin /pedidos/{id} disponibilizado

---
+++ src/main/java/com/carolaine/cursomod_conceitual/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.carolaine.cursomod_conceitual.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carolaine.cursomod_conceitual.domain.Pedido;
+import com.carolaine.cursomod_conceitual.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName(), obj));
+		}
+}
+
+
