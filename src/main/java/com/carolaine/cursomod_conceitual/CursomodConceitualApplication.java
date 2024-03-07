3d8b58f28fd1fb13d3574a32eed29608fcf3152b
Carolaine Machado <carolainemachado12345@gmail.com>
2024-03-07 14:24:13 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/carolaine/cursomod_conceitual/CursomodConceitualApplication.java
+++ src/main/java/com/carolaine/cursomod_conceitual/CursomodConceitualApplication.java
@@ -1,5 +1,6 @@
 package com.carolaine.cursomod_conceitual;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.carolaine.cursomod_conceitual.domain.Cidade;
 import com.carolaine.cursomod_conceitual.domain.Cliente;
 import com.carolaine.cursomod_conceitual.domain.Endereco;
 import com.carolaine.cursomod_conceitual.domain.Estado;
+import com.carolaine.cursomod_conceitual.domain.Pagamento;
+import com.carolaine.cursomod_conceitual.domain.PagamentoComBoleto;
+import com.carolaine.cursomod_conceitual.domain.PagamentoComCartao;
+import com.carolaine.cursomod_conceitual.domain.Pedido;
 import com.carolaine.cursomod_conceitual.domain.Produto;
+import com.carolaine.cursomod_conceitual.domain.enums.EstadoPagamento;
 import com.carolaine.cursomod_conceitual.domain.enums.TipoCliente;
 import com.carolaine.cursomod_conceitual.repositories.CategoriaRepository;
 import com.carolaine.cursomod_conceitual.repositories.CidadeRepository;
 import com.carolaine.cursomod_conceitual.repositories.ClienteRepository;
 import com.carolaine.cursomod_conceitual.repositories.EnderecoRepository;
 import com.carolaine.cursomod_conceitual.repositories.EstadoRepository;
+import com.carolaine.cursomod_conceitual.repositories.PagamentoRepository;
+import com.carolaine.cursomod_conceitual.repositories.PedidoRepository;
 import com.carolaine.cursomod_conceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomodConceitualApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomodConceitualApplication.class, args);
@@ -87,6 +99,24 @@ public class CursomodConceitualApplication implements CommandLineRunner{
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, 
+		sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 
